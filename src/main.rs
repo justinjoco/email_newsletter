2e43f3be0c11d65fc353e4f37173abca0bda17be
Justin Joco <justin.joco@gmail.com>
2024-10-06 19:03:27 -0700

    tests now work due to changing test directory name to "tests"

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::net::TcpListener;
 use zero2prod::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("listener should have address binded");
+    run(listener)?.await
 }
