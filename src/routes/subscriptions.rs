5c4315a1ccebef2d31abbd223aa156895b4b2d77
Justin Joco <justin.joco@gmail.com>
2024-10-10 21:42:57 -0700

    Fixed user and secret values

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,10 +1,11 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgConnection;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
     _email: String,
     _name: String,
 }
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(_form: web::Form<FormData>, _connection: web::Data<PgConnection>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
