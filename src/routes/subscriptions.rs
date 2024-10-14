67499fc3c9e3b1b8870755135129343e2b8edf47
Justin Joco <justin.joco@gmail.com>
2024-10-13 23:36:57 -0700

    Formatting

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,8 +14,6 @@ pub struct FormData {
     name: String,
 }
 
-
-#[allow(clippy::all)]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     match sqlx::query!(
         r#"INSERT INTO subscriptions(id, email, name, subscribed_at) VALUES($1, $2, $3, $4);"#,
